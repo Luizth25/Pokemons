59404ad04038671807b1653b6adcfa0f39774513
Luizth25 <luizftp25@gmail.com>
2022-08-24 16:05:39 -0300

    Roboto

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -6,6 +6,7 @@ export default createGlobalStyle`
   padding:0;
   box-sizing:border-box;
   list-style: none;
+  font-family: "Roboto", sans-serif
 }
 body{
   background-color:${({ theme }) => theme.colors.background}
